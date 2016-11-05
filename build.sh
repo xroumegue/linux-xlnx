7be802a46061b851a3e5bb2fc04705a9ab8526cb
Xavier Roumegue <xroumegue@gmail.com>
2016-11-05 01:00:29 +0100

    FOO
    Signed-off-by: Xavier Roumegue <xroumegue@gmail.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=arm-xilinx-linux-gnueabi- 
+export CROSS_COMPILE=arm-xilinx-linux-gnueabi-
 make zynq_zturn_defconfig
 make clean
 make  -j12 UIMAGE_LOADADDR=0x8000  uImage zynq-zturn.dtb
